da5f58a6b169e0ca6beebc2b1f571d1bdba11bbb
Tiku Okoye <tiku.okoye@brank.as>
2020-10-28 13:30:07 +0100

    - Updates codebase
    - Updates codebase to allow upload of folders
    - Changes the name of script to test test_notifier
    - Updates configuration of environment variables

--- test_notifier.py
+++ test_notifier.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python3
 
 import os
+import glob
 import sys
 import requests
 import json
 from google.cloud import storage
 
 
-def upload_html():
-    """Uploads a file to the bucket."""
-    html_path = os.environ.get('HTML_PATH')
-    gcs_bucket = os.environ.get('GCS_BUCKET')
-    destination_blob_name = os.environ.get('BLOB_NAME')
-    
+def upload_html(html_path, gcs_bucket, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcs_bucket)
     try:
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(gcs_bucket)
-        blob = bucket.blob(destination_blob_name)
-        blob.upload_from_filename(html_path)
+        if os.path.isdir(html_path):
+            for local_file in glob.glob(html_path + '/**'):
+                remote_path = os.path.join(destination_blob_name, local_file[0 + len(html_path):])
+                blob = bucket.blob(remote_path)
+                blob.upload_from_filename(local_file)
 
-        print(
-            "File {} uploaded to {}.".format(
-                html_path, destination_blob_name
+                print(
+                    "File {} uploaded to {}.".format(
+                        local_file, destination_blob_name
+                    )
+                )
+            
+            bucket_url = 'https://storage.googleapis.com/%s/%s/index.html' %(gcs_bucket, destination_blob_name)
+            return  bucket_url
+        else:
+            blob = bucket.blob(destination_blob_name)
+            blob.upload_from_filename(html_path)
+
+            print(
+                "File {} uploaded to {}.".format(
+                    html_path, destination_blob_name
+                )
             )
-        )
-        print(blob.public_url)
-        return blob.public_url
+            return blob.public_url
     except Exception as e:
         print("Unexpected error:", e)
 
 
-def post_to_slack(slack_text):
-    slack_channel = os.environ.get('SLACK_CHANNEL')
+def post_to_slack(slack_channel, slack_text, build_id, project_id, webhook_url):
     slack_user_name = 'E2E Test Report'
-    build_id = os.environ.get('BUILD_ID')
-    project_id = os.environ.get('PROJECT_ID')
-    webhook_url = os.environ.get('WEBHOOK_URL')
     cloudbuild_url = 'https://console.cloud.google.com/cloud-build/builds/%s?project=%s' %(build_id, project_id)
     slack_data = {
         'channel': slack_channel,
@@ -86,10 +92,20 @@ def post_to_slack(slack_text):
 
 
 def main():
-    test_url = upload_html()
-    post_to_slack(test_url)
+    # Get environment variables
+    html_path = os.environ.get('HTML_PATH')
+    gcs_bucket = os.environ.get('GCS_BUCKET')
+    destination_blob_name = os.environ.get('BLOB_NAME')
+    slack_channel = os.environ.get('SLACK_CHANNEL')
+    build_id = os.environ.get('BUILD_ID')
+    project_id = os.environ.get('PROJECT_ID')
+    webhook_url = os.environ.get('WEBHOOK_URL')
+
+    # upload test file/folders to GCS
+    test_url = upload_html(html_path, gcs_bucket, destination_blob_name)
+
+    # post notification to slack
+    post_to_slack(slack_channel, test_url, build_id, project_id, webhook_url)
 
 if __name__ == "__main__":
     main()
-
-
