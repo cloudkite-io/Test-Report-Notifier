b64e18ba6852838bea85868d08abb93c33125e30
Tiku Okoye <tiku.okoye@brank.as>
2020-10-30 17:49:27 +0100

    replace bucket url

--- test_notifier.py
+++ test_notifier.py
@@ -11,11 +11,11 @@ from google.cloud import storage
 def upload_html(html_path, gcs_bucket, destination_blob_name, project_id):
     storage_client = storage.Client()
     bucket = storage_client.bucket(gcs_bucket)
+
     try:
         if os.path.isdir(html_path):
             for local_file in glob.glob(html_path + '/**'):
                 remote_path = '%s%s' %(destination_blob_name, local_file[0 + len(html_path):])
-                print(destination_blob_name, remote_path)
                 blob = bucket.blob(remote_path)
                 blob.upload_from_filename(local_file)
 
@@ -25,7 +25,13 @@ def upload_html(html_path, gcs_bucket, destination_blob_name, project_id):
                     )
                 )
             
-            bucket_url = 'https://console.cloud.google.com/storage/browser/%s/%s?project=%s' %(gcs_bucket, destination_blob_name, project_id)
+            if os.path.isfile(html_path + '/index.html'):
+                bucket_url = 'https://storage.googleapis.com/%s/%s/index.html' %(gcs_bucket, destination_blob_name)
+            elif os.path.isfile(html_path + '/index.htm'):
+                bucket_url = 'https://storage.googleapis.com/%s/%s/index.htm' %(gcs_bucket, destination_blob_name)
+            else:
+                bucket_url = 'https://storage.googleapis.com/%s/%s' %(gcs_bucket, destination_blob_name)
+
             return  bucket_url
         else:
             blob = bucket.blob(destination_blob_name)
@@ -124,12 +130,12 @@ def main():
     repo_name = os.environ.get('REPO_NAME')
     branch_name = os.environ.get('BRANCH_NAME')
     commit = os.environ.get('COMMIT_SHA')
-    
+
     # upload test file/folders to GCS
     test_url = upload_html(html_path, gcs_bucket, destination_blob_name, project_id)
 
-    # post notification to slack
-    post_to_slack(slack_channel, test_url, build_id, project_id, webhook_url, repo_name, branch_name, commit)
+    # # post notification to slack
+    # post_to_slack(slack_channel, test_url, build_id, project_id, webhook_url, repo_name, branch_name, commit)
 
 if __name__ == '__main__':
     main()
